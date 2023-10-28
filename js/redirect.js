d73d56efb23144b7507928a6f3df36de75fa7238
vitor.dev <146399948+its-vitor@users.noreply.github.com>
2023-10-28 14:57:55 -0300

    Delete js directory

--- js/redirect.js
+++
@@ -1,8 +0,0 @@
-function checkAndRedirect() {
-    var inputLink = document.getElementById('link-input').value;
-    if (inputLink.includes("aminoapps.com")) {
-        window.location.href = inputLink;
-    } else {
-        alert("O seu link de perfil não é válido.");
-    }
-}
